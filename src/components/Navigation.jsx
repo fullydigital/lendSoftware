46b81c98585e0438d7d2558a5b778d8a3b6ed886
Michael <m_reinert@outlook.com>
2023-11-09 17:58:11 +0100

    changed some things

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,11 +6,11 @@ export default function Navigation() {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-red-600 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
+            <p
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
             >
               Sport Weber Schnaittach
-            </a>
+            </p>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -28,25 +28,25 @@ export default function Navigation() {
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
-                <a
+                <p
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                 >
                   <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Ski</span>
-                </a>
+                </p>
               </li>
               <li className="nav-item">
-                <a
+                <p
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                 >
                   <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Bike</span>
-                </a>
+                </p>
               </li>
               <li className="nav-item">
-                <a
+                <p
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Zubehör</span>
-                </a>
+                </p>
               </li>
             </ul>
           </div>
