5e22ea11c8a221f242ecedb649e28aa7a0104527
Michael Reinert <michael.reinert@wesolve-it.com>
2025-01-27 19:41:47 +0100

    optimized date picking for no double bookings

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Alert() {
   return (
-    <div className='mx-auto mb-10 h-20 w-80 bg-red-200 text-black'><p className='pt-6'>Bitte eine Größe wählen</p></div>
+    <div className='mx-auto mb-10 h-20 bg-red-200 text-black'><p className='pt-6'>Bitte eine Größe wählen</p></div>
+  )
+}
+
+export function BookedAlert() {
+  return (
+    <div className='mx-auto mb-10 h-20 bg-red-200 text-black'><p className='pt-4'>Der Artikel ist in diesem Zeitraum nicht verfügbar</p></div>
   )
 }
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,6 +3,7 @@ import Select from "react-select";
 import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css'
 import Alert from './Alert';
+import { BookedAlert } from './Alert';
 import ArticleBooked from './ArticleBooked';
 
 export default function Article({item, bookings}) {
@@ -10,6 +11,7 @@ export default function Article({item, bookings}) {
   const [deleteStartDate, setDeleteStartDate] = useState([]);
   const [booked, setBooked] = useState(false);
   const [alert, setAlert] = useState(false);
+  const [bookedAlert, setBookedAlert] = useState(false);
   const [articleBooked, setArticleBooked] = useState(false);
   let cart = JSON.parse(localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')) : [];
 
@@ -21,93 +23,89 @@ export default function Article({item, bookings}) {
 
   useEffect(() => {
     let array = [];
-      bookings.map((booking) => {
-        if (booking.size.id === article.id) {
-          let currentDate = new Date(booking.startDate);
-          while (currentDate <= new Date(booking.endDate)) {
-            array.push(new Date(currentDate));
-            currentDate.setDate(currentDate.getDate() + 1);
-          }
-          if (currentDate >= new Date(currentDate.getDate() - 1)) {
-            array.push(new Date(currentDate.getDate() - 1))
-          }
+    bookings.map((booking) => {
+      if (booking.size.id === article.id) {
+        let currentDate = new Date(booking.startDate);
+        while (currentDate <= new Date(booking.endDate)) {
+          array.push(new Date(currentDate));
+          currentDate.setDate(currentDate.getDate() + 1);
         }
-        return true;
       }
-    )
+      return true;
+    })
     setDeleteStartDate(array);
   }, [article, bookings])
 
   const handleChange = (range) => {
     const [startDate, endDate] = range;
-    setArticle({...article, startDate: startDate, endDate: endDate, name: item.name, image: item.image, pricePerDay: price})
+    const isOverlapping = deleteStartDate.some(date => 
+      (date >= startDate && date <= endDate)
+    );
+
+    if (!isOverlapping) {
+      setArticle({...article, startDate: startDate, endDate: endDate, name: item.name, image: item.image, pricePerDay: price});
+    } else {
+      setBookedAlert(true);
+    }
   }
 
-    const handleClick = () => {
-      if(article.size !== 0) {
-        if(cart.length > 0) {
-          var checkCart = cart.filter(item => item.id === article.id);
-          if (checkCart.length > 0) {
-            setArticleBooked(true);
-          } else {
-            cart.push(article);
-            localStorage.setItem('cart', JSON.stringify(cart))
-          }
-        }
-        else {
+  const handleClick = () => {
+    if(article.size !== 0) {
+      if(cart.length > 0) {
+        var checkCart = cart.filter(item => item.id === article.id);
+        if (checkCart.length > 0) {
+          setArticleBooked(true);
+        } else {
           cart.push(article);
-          localStorage.setItem('cart', JSON.stringify(cart))
-          setBooked(true)
         }
       } else {
-        setAlert(true);
+        cart.push(article);
       }
-      setTimeout(() => {
-        setBooked(false);
-      }, 2000)
-      setTimeout(() => {
-        setAlert(false);
-        setArticleBooked(false);
-      }, 5000)
+      localStorage.setItem('cart', JSON.stringify(cart));
+      setBooked(true);
+    } else {
+      setAlert(true);
     }
+  }
 
-    if (!item) return "Loading...";
+  if (!item) return "Loading...";
 
-    return (
-      <div className="mt-10 border-2 w-5/6 mx-auto py-10 lg:w-5/12 xl:w-3/12 rounded-xl px-3">
-        {item.image ? 
-        <img className="w-52 h-72 mx-auto mb-6 object-contain" src={item.image}
-             alt="Bild von dem Objekt"/> : <img className="w-52 h-52 mx-auto mb-6 object-cover" src={require('../assets/bannskirent.webp')}
-             alt="Platzhalterbild"/>}
-        <div className="flex-col flex mb-4">
-          <p className="mb-2 font-semibold">Artikel:</p>
-          <p>{item.name}</p>
-        </div>
-        <div className="flex-col flex mb-4">
-          <p className="font-semibold mb-2">Größe: </p>
-          <Select className="w-4/6 mx-auto" value={article.size} onChange={setArticle} options={item.sizes}/>
-        </div>
-        <div className="mb-4">
-          <p className="font-semibold mb-2">Preis pro Tag:</p>
-          <p>{price} €</p>
-        </div>
-        <div className="flex-col flex mb-10">
-          <p className="font-semibold mb-2">Tage wählen:</p>
-          <DatePicker
-            className="border-2 rounded-lg w-4/6 py-3 text-center"
-            selected={article.startDate}
-            onChange={handleChange}
-            startDate={article.startDate}
-            endDate={article.endDate}
-            selectsRange
-            dateFormat="dd.M.yy"
-            excludeDates={deleteStartDate}
-            minDate={new Date()}
-          />
-        </div>
-        {alert ? <Alert /> : null}
-        {articleBooked ? <ArticleBooked /> : null}
-        <button onClick={handleClick} className="bg-red-600 text-white w-4/6 rounded-lg py-3">{booked ? 'Hinzugefügt' : 'In den Warenkorb'}</button>
+  return (
+    <div className="mt-10 border-2 w-5/6 mx-auto py-10 lg:w-5/12 xl:w-3/12 rounded-xl px-3">
+      {item.image ? 
+      <img className="w-52 h-72 mx-auto mb-6 object-contain" src={item.image}
+           alt="Bild von dem Objekt"/> : <img className="w-52 h-52 mx-auto mb-6 object-cover" src={require('../assets/bannskirent.webp')}
+           alt="Platzhalterbild"/>}
+      <div className="flex-col flex mb-4">
+        <p className="mb-2 font-semibold">Artikel:</p>
+        <p>{item.name}</p>
       </div>
-    )
+      <div className="flex-col flex mb-4">
+        <p className="font-semibold mb-2">Größe: </p>
+        <Select className="w-4/6 mx-auto" value={article.size} onChange={setArticle} options={item.sizes}/>
+      </div>
+      <div className="mb-4">
+        <p className="font-semibold mb-2">Preis pro Tag:</p>
+        <p>{price} €</p>
+      </div>
+      <div className="flex-col flex mb-10">
+        <p className="font-semibold mb-2">Tage wählen:</p>
+        <DatePicker
+          className="border-2 rounded-lg w-4/6 py-3 text-center"
+          selected={article.startDate}
+          onChange={handleChange}
+          startDate={article.startDate}
+          endDate={article.endDate}
+          selectsRange
+          dateFormat="dd.M.yy"
+          excludeDates={deleteStartDate}
+          minDate={new Date()}
+        />
+      </div>
+      {alert ? <Alert /> : null}
+      {bookedAlert ? <BookedAlert /> : null}
+      {articleBooked ? <ArticleBooked /> : null}
+      <button onClick={handleClick} className="bg-red-600 text-white w-4/6 rounded-lg py-3">{booked ? 'Hinzugefügt' : 'In den Warenkorb'}</button>
+    </div>
+  )
 }
