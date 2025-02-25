689b89dd3031d06535c2c505f5146448ab506685
Michael Reinert <michael.reinert@wesolve-it.com>
2025-02-25 18:18:30 +0100

    refactor AdminPage: optimize booking grouping and pagination logic

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -5,7 +5,6 @@ import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css'
 
-
 export default function AdminPage() {
   const [bookings, setBookings] = useState(null);
   const [sortedBookings, setSortedBookings] = useState([]);
@@ -19,7 +18,6 @@ export default function AdminPage() {
     endDate: new Date(),
     startDate: new Date(),
   })
-  
 
   const query = `
     {
@@ -117,13 +115,6 @@ export default function AdminPage() {
     setCurrentPage(pageNumber);
   }
 
-  // Pagination: Berechne die Einträge für die aktuelle Seite
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = sortedBookings.slice(indexOfFirstItem, indexOfLastItem);
-
-  const totalPages = Math.ceil(sortedBookings.length / itemsPerPage);
-
   const handleChange = (range) => {
     const [startDate, endDate] = range;
     setTime({startDate: startDate, endDate: endDate})
@@ -156,15 +147,6 @@ export default function AdminPage() {
       alert('Dieser Name ist nicht vorhanden!')
     }
   }
-
-  // const searchForDate = () => {
-  //   var newArray = sortedBookings.filter((item) => new Date(item.startDate) >= time.startDate && new Date(item.endDate) <= time.endDate)
-  //   if (newArray.length > 0) {
-  //     setSortedBookings(newArray);
-  //   } else {
-  //     alert('Für den Zeitraum sind keine Daten vorhanden!')
-  //   }
-  // }
 
   const searchForDate = () => {
     const startDate = new Date(time.startDate);
@@ -203,17 +185,6 @@ export default function AdminPage() {
   const renderPagination = () => {
     const visiblePages = getVisiblePages(currentPage, totalPages);
 
-    // for (let i = 1; i <= totalPages; i++) {
-    //   pages.push(
-    //     <button
-    //       key={i}
-    //       onClick={() => handlePageChange(i)}
-    //       className={`px-2 py-1 mx-1 rounded ${i === currentPage ? 'bg-red-600 text-white' : 'bg-gray-300'}`}
-    //     >
-    //       {i}
-    //     </button>
-    //   );
-    // }
     return (
       <div className="flex justify-center mt-4">
         {visiblePages.map((page, index) =>
@@ -235,23 +206,41 @@ export default function AdminPage() {
         )}
       </div>
     );
-  
   };
 
   if (!bookings) return "Keine Einträge vorhanden";
 
+  // Gruppiere Buchungen nach Vorname, Nachname und Buchungsdatum
+  const groupedBookings = sortedBookings.reduce((acc, booking) => {
+    const key = `${booking.firstName}-${booking.lastName}-${booking.bookingDate}`;
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(booking);
+    return acc;
+  }, {});
+
+  const groupedItems = Object.values(groupedBookings);
+
+  // Pagination: Berechne die Einträge für die aktuelle Seite
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = groupedItems.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(groupedItems.length / itemsPerPage);
+
   return (
     <div>
-    <div className="w-11/12 ml-4 mt-12 mb-20">
-      <p className='font-semibold mb-6'>Filter:</p>
-      <div className='mb-6'>
-        <label className='mr-10'>Nachname:</label>
-      <input onChange={(e) => setNameSearch(e.target.value)} placeholder='Nachname...' type='text' className='py-2 text-left px-10 rounded-lg border-gray-200 border-solid border-2' />
-      <button onClick={searchForName} className='ml-10 bg-red-600 px-4 py-2 rounded-lg text-white'>Suchen</button>
-      </div>
-      <div className='mb-6'>
-        <label className='mr-10'>Datum:</label>
-        <DatePicker
+      <div className="w-11/12 ml-4 mt-12 mb-20">
+        <p className='font-semibold mb-6'>Filter:</p>
+        <div className='mb-6'>
+          <label className='mr-10'>Nachname:</label>
+          <input onChange={(e) => setNameSearch(e.target.value)} placeholder='Nachname...' type='text' className='py-2 text-left px-10 rounded-lg border-gray-200 border-solid border-2' />
+          <button onClick={searchForName} className='ml-10 bg-red-600 px-4 py-2 rounded-lg text-white'>Suchen</button>
+        </div>
+        <div className='mb-6'>
+          <label className='mr-10'>Datum:</label>
+          <DatePicker
             className="border-2 rounded-lg py-3 text-center"
             selected={time.startDate}
             onChange={handleChange}
@@ -260,49 +249,52 @@ export default function AdminPage() {
             selectsRange
             dateFormat="dd.M.yy"
           />
-      <button className='ml-10 bg-red-600 px-4 py-2 rounded-lg text-white' onClick={searchForDate}>Suchen</button>
-      </div>
-      <button className='mb-12 bg-red-600 px-6 py-2 rounded-lg text-white' onClick={handleSetback}>Zurücksetzen</button>
-    <Table className="table-auto border-collapse border-2" key={1}>
-      <Thead>
-      <Tr>
-          {Object.keys(bookings[0]).map((key) => (
-            <React.Fragment key={key}>
-              <Th className="border-2">{key}</Th>
-              {key === 'size' ? <Th className="border-2">Artikelname</Th> : null}
-              </React.Fragment>
+          <button className='ml-10 bg-red-600 px-4 py-2 rounded-lg text-white' onClick={searchForDate}>Suchen</button>
+        </div>
+        <button className='mb-12 bg-red-600 px-6 py-2 rounded-lg text-white' onClick={handleSetback}>Zurücksetzen</button>
+        <Table className="table-auto border-collapse border-2" key={1}>
+          <Thead>
+            <Tr>
+              {Object.keys(bookings[0]).map((key) => (
+                <React.Fragment key={key}>
+                  {key !== 'id' && key !== 'size' && key !== 'articleSet' && (
+                    <Th className="border-2">{key}</Th>
+                  )}
+                </React.Fragment>
               ))}
-          <Th>Rechnung</Th>
-              </Tr>
-      </Thead>
-      <Tbody>
-      {currentItems.map((item) => (
-          <Tr key={item.id}>
-            {Object.values(item).map((val) => (
-              <React.Fragment key={val?.id || Math.random()}>
-              <Td className="border-2">
-                {val?.label 
-                  ? val.label 
-                  : val?.articleSet?.[0]?.name || val}
-              </Td>
-              {val?.label && (
+              <Th>Rechnung</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {currentItems.map((group, index) => (
+              <Tr key={index}>
+                {Object.entries(group[0]).map(([key, val], idx) => (
+                  <React.Fragment key={idx}>
+                    {key !== 'id' && key !== 'size' && key !== 'articleSet' && (
+                      <Td className="border-2">
+                        {key === 'bookingDate' || key === 'startDate' || key === 'endDate'
+                          ? new Date(val).toLocaleDateString('de-DE')
+                          : val?.label 
+                            ? val.label 
+                            : val?.articleSet?.[0]?.name || val}
+                      </Td>
+                    )}
+                  </React.Fragment>
+                ))}
                 <Td className="border-2">
-                  {val?.articleSet?.[0]?.name || null}
+                  <button className="bg-red-600 py-1 px-2 rounded-lg text-white" onClick={() => {handleInvoice(group[0].id); setShowInvoice(!showInvoice)}}>
+                    Rechnung
+                  </button>
                 </Td>
-              )}
-            </React.Fragment>
+              </Tr>
             ))}
-          <Td className="border-2"><button className="bg-red-600 py-1 px-2 rounded-lg text-white" onClick={() => {handleInvoice(item.id); setShowInvoice(!showInvoice)}}>Rechnung</button></Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
+          </Tbody>
+        </Table>
 
-{/* Pagination Controls */}
+        {/* Pagination Controls */}
         {renderPagination()}
-
-    </div>
-    {showInvoice ? <Invoice itemId={itemId} bookings={bookings} /> : null}
+      </div>
+      {showInvoice ? <Invoice itemId={itemId} bookings={bookings} /> : null}
     </div>
   )
 }
